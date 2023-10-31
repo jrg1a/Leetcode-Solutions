4e5ee277de511d8504af505024cfac45e74da897
Jørgen Austnes <42777752+Austnez@users.noreply.github.com>
2023-10-31 20:27:12 +0100

    upload SearchInsertPosition.java

---
+++ SearchInsertPosition.java
@@ -0,0 +1,19 @@
+// Search Insert Position Problem - Leetcode ( https://leetcode.com/problems/search-insert-position/description/ )
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int srch = 0;
+        int e = nums.length-1;
+        while( srch<= e ) {
+            int mid = srch+(e-srch)/2;
+            if (target>nums[mid]) {
+                srch=mid+1;
+            }
+            else if(target<nums[mid]) {
+                e = mid-1;
+            } else {
+                return mid;
+            }    
+        }
+        return srch;
+    }
+}
